cdbca2eec0ccaf014bb275004caf40507085888c
clojj <jwin1968@gmail.com>
2016-10-11 23:06:22 +0200

    refactorings, simplify model

--- webclient/src/Main.elm
+++ webclient/src/Main.elm
@@ -1,11 +1,12 @@
 module Main exposing (..)
 
-import Html exposing ( Html )
+import Html exposing (Html)
 import Html.App as Html
 import Http
 import Task exposing (..)
 import Debug
 import String exposing (..)
+
 
 -- import Html.Attributes exposing (..)
 -- import Html.Events exposing (onClick)
@@ -14,8 +15,8 @@ import HvCss
 import Html.CssHelpers
 
 
-{ id, class, classList } = Html.CssHelpers.withNamespace "haskell-view"
-
+{ id, class, classList } =
+    Html.CssHelpers.withNamespace "haskell-view"
 main : Program Never
 main =
     Html.program
@@ -25,76 +26,94 @@ main =
         , subscriptions = subscriptions
         }
 
+
+
 -- MODEL
 
-type alias Location =
-  { line : Int
-  , col : Int
-  }
-
-type alias Token =
-  { name : String
-  , from : Location
-  , to : Location
-  }
 
 type alias Model =
-    { src : String
-    , html : Html Msg
-    , tokens : String
+    { html : Html Msg }
+
+
+type alias Location =
+    { line : Int
+    , col : Int
+    }
+
+
+type alias Token =
+    { name : String
+    , from : Location
+    , to : Location
     }
 
 
 initialModel : Model
 initialModel =
-  { src = ""
-  , html = Html.text ""
-  , tokens = ""
-  }
+    { html = Html.text "initializing.." }
 
 
 init : ( Model, Cmd Msg )
-init = ( initialModel, getHaskell "Lib" )
+init =
+    ( initialModel, getHaskell "Lib" )
+
 
 
 -- UPDATE
 
+
 type Msg
-  = GetHaskell
-  | FetchSucceed String
-  | FetchFail Http.Error
+    = GetHaskell
+    | GetHaskellSucceed String
+    | FetchFail Http.Error
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-
         GetHaskell ->
-          (model, getHaskell "TODO")
+            ( model, getHaskell "TODO" )
 
-        FetchSucceed data ->
-           case (split "<EOF>" data) of
-            [] -> (Model "err" (Html.text "err") "", Cmd.none)
-            src :: tokens ->
-              let t = case (List.head tokens) of
-                        Just ts -> ts
-                        Nothing -> "no tokens"
-                  html = createView src t
-                  in ((Model model.src (Debug.log "HTML\n" html) model.tokens), Cmd.none)
+        GetHaskellSucceed data ->
+            case (split "<EOF>" data) of
+                [] ->
+                    ( Model (Html.text "error"), Cmd.none )
+
+                src :: rest ->
+                    let
+                        tokens =
+                            case (List.head rest) of
+                                Just ts ->
+                                    ts
+
+                                Nothing ->
+                                    "no tokens"
+
+                        html =
+                            createView src tokens
+                    in
+                        ( (Model (Debug.log "HTML\n" html)), Cmd.none )
 
         FetchFail _ ->
-          (model, Cmd.none)
+            ( model, Cmd.none )
 
--- HTTP
+
+
+-- perform HTTP
+
 
 getHaskell : String -> Cmd Msg
 getHaskell path =
-  let url = "http://localhost:8081/source/" ++ path
-  in
-    Task.perform FetchFail FetchSucceed <| Http.getString url
+    let
+        url =
+            "http://localhost:8081/source/" ++ path
+    in
+        Task.perform FetchFail GetHaskellSucceed <| Http.getString url
 
 
 
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -104,17 +123,24 @@ subscriptions model =
 
 -- VIEW
 
+
 view : Model -> Html Msg
-view { src, html, tokens } = html
+view { html } =
+    html
+
+
+
+-- create Html from update
+
 
 createView : String -> String -> Html Msg
 createView src tokens =
-    Html.div [] [
-      Html.div [] (List.map viewLine (split "\n" src)),
-      Html.div [] [Html.text ("TOKENS: " ++ tokens)]
-    ]
+    Html.div []
+        [ Html.div [] (List.map viewLine (split "\n" src))
+        , Html.div [] [ Html.text ("TOKENS: " ++ tokens) ]
+        ]
+
 
 viewLine : String -> Html Msg
 viewLine line =
-    Html.div [ class [HvCss.ITlineComment]] [ Html.text line, Html.br [] []]
-
+    Html.div [ class [ HvCss.ITlineComment ] ] [ Html.text line, Html.br [] [] ]

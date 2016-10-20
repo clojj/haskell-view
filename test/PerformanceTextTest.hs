ddb7e47adec6e3932e4702c860399dfe77a109cf
clojj <jwin1968@gmail.com>
2016-10-20 09:12:44 +0200

    fix ls

--- test/PerformanceTextTest.hs
+++ test/PerformanceTextTest.hs
@@ -16,13 +16,13 @@ type Acc  = ((Int, Int), T.Text)
 
 f :: (Int, [Int]) -> Char -> (Int, [Int])
 f (offset, xs) c = case c of
-                        '\n' -> (offset + 1, offset : xs)
+                        '\n' -> (offset + 1, offset + 1 : xs)
                         _    -> (offset + 1, xs)
 
 doText :: T.Text -> [Token] -> T.Text
 doText src ts =
-  let ls = V.fromList $ -1 : (snd $ T.foldl' f (0, []) src)
-      result = L.foldl' (foldText ls src) ((1, 0), T.empty) ts
+  let ls = V.fromList $ L.reverse $ snd $ T.foldl' f (-1, [-1]) src
+      result = L.foldl' (foldText ls src) ((1, 1), T.empty) ts
       lastOff = toOffset ls (fst result)
       end = T.drop (lastOff + 1) src
   in
@@ -34,6 +34,6 @@ foldText ls src ((l, c), src') (((l1, c1), (l2, c2)), token) =
     ws      = substrText src ls (l, c) 1 (l1, c1) 0
     lexeme  = substrText src ls (l1, c1) 0 (l2, c2) 1
   in
-    ((l2, c2), (T.append (T.append (T.append (T.append (T.append (T.append src' ws) ":") token) ":") lexeme) ":"))
-    -- ((l2, c2), src' <> ws <> ":" <> token <> ":" <> lexeme <> ":")
+    -- ((l2, c2), (T.append (T.append (T.append (T.append (T.append (T.append src' ws) ":") token) ":") lexeme) ":"))
+    ((l2, c2), src' <> ws <> ":" <> token <> ":" <> lexeme <> ":")
 

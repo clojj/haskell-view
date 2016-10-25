0ad039f74355a59f01ea52e94e677117f938f25c
clojj <jwin1968@gmail.com>
2016-10-25 21:45:22 +0200

    more refactoring for position type

--- test/PerformanceFoldOverCharsTest.hs
+++ test/PerformanceFoldOverCharsTest.hs
@@ -9,37 +9,37 @@ import           Debug.Trace
 import Lib
 
 type Token  = (TokenSpan, String)
-type Acc = ([Token], Seq Char, Int, Int)
+type Acc = ([Token], Seq Char, LineColumnPos)
 
 doFoldOverChars :: T.Text -> [Token] -> Seq Char
 doFoldOverChars src ts =
   let
-    (_, src', _, _) = T.foldl' f (ts, empty, 1, 1) src
+    (_, src', _) = T.foldl' f (ts, empty, Pos 1 1) src
   in
     src'
 
 f :: Acc -> Char -> Acc
-f (ts, src, l, c) ch =
+f (ts, src, p@(Pos l c)) ch =
   let (l', c') = case ch of
                    '\n' -> (l + 1, 1)
                    _    -> (l, c + 1)
   in
     case ts of
       [] ->
-        ([], src |> ch, l, c)
+        ([], src |> ch, p)
 
       (((p1@(Pos l1 c1), p2@(Pos l2 c2)), t1) : (_, t2) : tokens) ->
-        if l1 == l && c1 == c then
+        if p1 == p then
             if p1 == p2 then
-              (tokens, (src |> ':') >< (fromList t1 |> ':') >< (fromList t2 |> ':' |> ch), l', c')
+              (tokens, (src |> ':') >< (fromList t1 |> ':') >< (fromList t2 |> ':' |> ch), Pos l' c')
             else
-              (tail ts, (src |> ':') >< (fromList t1 |> ':' |> ch), l', c')
+              (tail ts, (src |> ':') >< (fromList t1 |> ':' |> ch), Pos l' c')
         else
-          (ts, src |> ch, l', c')
+          (ts, src |> ch, Pos l' c')
 
       (((p1@(Pos l1 c1), _), t1) : tokens) ->
-        if l1 == l && c1 == c then
-          (tokens, (src |> ':') >< (fromList t1 |> ':' |> ch), l', c')
+        if p1 == p then
+          (tokens, (src |> ':') >< (fromList t1 |> ':' |> ch), Pos l' c')
         else
-          (ts, src |> ch, l', c')
+          (ts, src |> ch, Pos l' c')
 

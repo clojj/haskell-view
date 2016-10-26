29aeb2618d5707d889c23e0a135460b8635ff0ea
clojj <clojj@users.noreply.github.com>
2016-10-26 12:32:41 +0200

    fix 0-length special tokens

--- test/PerformanceFoldOverCharsTest.hs
+++ test/PerformanceFoldOverCharsTest.hs
@@ -9,42 +9,51 @@ import           Debug.Trace
 import Lib
 
 type Token  = (TokenSpan, String)
-type Acc = ([Token], Seq Char, LineColumnPos)
+type Acc = ([Token], Seq Char, LineColumnPos, Maybe Seq Char)
 
 doFoldOverChars :: T.Text -> [Token] -> Seq Char
 doFoldOverChars src ts =
-  let
-    (_, src', _) = T.foldl' foldIt (ts, empty, Pos 1 1) src
+  let (_, src', _, _) = T.foldl' foldIt (ts, empty, Pos 1 1, Nothing) src
   in
     src'
 
 foldIt :: Acc -> Char -> Acc
-foldIt (ts, src, p@(Pos l c)) ch =
+foldIt (ts, src, p@(Pos l c), accChars) ch =
+
+  let accLength = case accChars of
+                    Just ac -> length ac
+                    Nothing -> 1
+                             
   let (l', c') = case ch of
-                   '\n' -> (l + 1, 1)
-                   _    -> (l, c + 1)
+                   '\n' -> (l + 1, accLength)
+                   _    -> (l, c + accLength)
+
   in
     case ts of
       [] ->
         ([], src |> ch, p)
 
-      (((p1@(Pos l1 c1), p2@(Pos l2 c2)), t1) : (_, t2) : tokens) ->
-        if p1 == p then
-          let token1 = (':' <| fromList t1) |> ':'
-              token2 = fromList t2 |> ':'
-          in
-            if p1 == p2 then
-              (tokens, (src >< token1 >< token2) |> ch, Pos l' c')
-            else
-              (tail ts, (src >< token1) |> ch, Pos l' c')
-        else
-          (ts, src |> ch, Pos l' c')
-
-      (((p1@(Pos l1 c1), _), t1) : tokens) ->
-          let token1 = (':' <| fromList t1) |> ':'
+      (((p1@(Pos l1 c1), p2@(Pos l2 c2)), t) : tokens) ->
+          let token = (':' <| fromList t) |> ':'
           in
             if p1 == p then
-              (tokens, (src >< token1) |> ch, Pos l' c')
+              if p1 == p2 then
+                (tokens, (src >< token), Pos l c, fmap (|> ch) accChars)
+              else
+                (tokens, (src >< token) |> ch, Pos l' c')
             else
               (ts, src |> ch, Pos l' c')
 
+
+
+--      (((p1@(Pos l1 c1), p2@(Pos l2 c2)), t1) : (_, t2) : tokens) ->
+--        if p1 == p then
+--          let token1 = (':' <| fromList t1) |> ':'
+--              token2 = fromList t2 |> ':'
+--          in
+--            if p1 == p2 then
+--              (tokens, (src >< token1 >< token2) |> ch, Pos l' c')
+--            else
+--              (tail ts, (src >< token1) |> ch, Pos l' c')
+--        else
+--          (ts, src |> ch, Pos l' c')

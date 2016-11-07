5939490be4f9c5784f532688196015a1d5135c3a
clojj <jwin1968@gmail.com>
2016-11-07 21:41:19 +0100

    add '-' postfix to whitespace and lexemes

--- test/PerformanceLoopByteStringUTF8Test.hs
+++ test/PerformanceLoopByteStringUTF8Test.hs
@@ -21,11 +21,6 @@ type Advancement  = (Int, Int)
 -- TODO type Offset = Int
 
 
-advanceLinesAndColumns :: LineColumnPos -> LineColumnPos -> Advancement
-advanceLinesAndColumns p1@(Pos l1 c1) p2@(Pos l2 c2)
-    | p1 == p2 = (0, 0)
-    | l2 - l1 > 0 = (l2 - l1, c2)
-    | otherwise = (0, c2 - c1)
 
 loopOverForElm :: ByteString -> Acc -> [Token] -> ByteString
 loopOverForElm bs (currentPos, result) tokens =
@@ -40,38 +35,46 @@ loopOverForElm bs (currentPos, result) tokens =
         if pos1 == pos2 then
           loopOverForElm bs (Pos l1 c1, result <> sepElmStart <> fromString tname <> sepElmEnd) tokenTail
         else
-          let advancement     = advanceLinesAndColumns pos1 pos2
-              (len, (token, bsTail)) = spanAdvancementElm advancement bs
-              -- (token, bsTail) = spanLines advancement bs
-          in loopOverForElm bsTail (Pos l2 c2, result <> sepElmStart <> fromString (show len) <> fromString (":" ++ tname) <> sepElmEnd <> token) tokenTail
+          let advancement             = advanceLinesAndColumns pos1 pos2
+              postFix                 = if advancesLines advancement then postFixLines else mempty
+              (len, (lexeme, bsTail)) = spanAdvancementElm advancement bs
+          in loopOverForElm bsTail (Pos l2 c2, result <> sepElmStart <> fromString (show len) <> fromString (":" ++ tname) <> postFix <> sepElmEnd <> lexeme) tokenTail
       else
-        let advancement  = advanceLinesAndColumns currentPos pos1
-            (len, (ws, bsTail)) = spanAdvancementElm advancement bs
-            -- (ws, bsTail) = spanLines advancement bs
-        in loopOverForElm bsTail (Pos l1 c1, result <> sepElmStart <> fromString (show len) <> fromString ":WS" <> sepElmEnd <> ws) tokens
+        let advancement                 = advanceLinesAndColumns currentPos pos1
+            postFix                     = if advancesLines advancement then postFixLines else mempty
+            (len, (whitespace, bsTail)) = spanAdvancementElm advancement bs
+        in loopOverForElm bsTail (Pos l1 c1, result <> sepElmStart <> fromString (show len) <> fromString ":WS" <> postFix <> sepElmEnd <> whitespace) tokens
 
-sepElmStart :: ByteString
-sepElmStart = fromString "["
+  where
+    advanceLinesAndColumns :: LineColumnPos -> LineColumnPos -> Advancement
+    advanceLinesAndColumns p1@(Pos l1 c1) p2@(Pos l2 c2)
+        | p1 == p2 = (0, 0)
+        | l2 - l1 > 0 = (l2 - l1, c2)
+        | otherwise = (0, c2 - c1)
 
-sepElmEnd :: ByteString
-sepElmEnd = fromString "]"
+    advancesLines :: Advancement -> Bool
+    advancesLines = (> 0) . fst
 
-spanAdvancementElm :: Advancement -> ByteString -> (Int, (ByteString, ByteString))
-spanAdvancementElm (l, c) bs
-  | l == 0 = (c, splitAt c bs)
-  -- | l > 0  = splitAt ((lineOffset l bs) + c - 1) bs
-  | l > 0  = splitAtElm (l, c - 1) bs
-spanAdvancementElm (_, _) _ = undefined
+    spanAdvancementElm :: Advancement -> ByteString -> (Int, (ByteString, ByteString))
+    spanAdvancementElm (l, c) bs
+      | l == 0 = (c, splitAt c bs)
+      -- | l > 0  = splitAt ((lineOffset l bs) + c - 1) bs
+      | l > 0  = splitAtElm (l, c - 1) bs
+    spanAdvancementElm (_, _) _ = undefined
 
--- LiquidHaskell: l > 0
-{-@ splitAtElm :: {v:_ | fst v > 0} -> B.ByteString -> (Int, (B.ByteString, B.ByteString)) @-}
+    -- LiquidHaskell: l > 0
+    {-@ splitAtElm :: {v:_ | fst v > 0} -> B.ByteString -> (Int, (B.ByteString, B.ByteString)) @-}
 
-splitAtElm :: Advancement -> B.ByteString -> (Int, (B.ByteString, B.ByteString))
-splitAtElm (ls, cs) bs = loop 0 (ls, cs) bs
-  where loop a (l, c) _ | (l, c) == (0, 0) = (a, B.splitAt a bs)
-        loop a (l, c) bs1 = case decode bs1 of
-                         Just (ch,y) -> case ch of
-                                          '\n' -> loop (a+y) (l-1, cs) (B.drop y bs1)
-                                          _    -> loop (a+y) (l, c-1) (B.drop y bs1)
-                         Nothing    ->  (0, (bs, B.empty))
+    splitAtElm :: Advancement -> B.ByteString -> (Int, (B.ByteString, B.ByteString))
+    splitAtElm (ls, cs) bs = loop 0 (ls, cs) bs
+      where loop a (l, c) _ | (l, c) == (0, 0) = (a, B.splitAt a bs)
+            loop a (l, c) bs1 = case decode bs1 of
+                             Just (ch,y) -> case ch of
+                                              '\n' -> loop (a+y) (l-1, cs) (B.drop y bs1)
+                                              _    -> loop (a+y) (l, c-1) (B.drop y bs1)
+                             Nothing    ->  (0, (bs, B.empty))
+
+    postFixLines = fromString "-"
+    sepElmStart  = fromString "["
+    sepElmEnd    = fromString "]"
 
